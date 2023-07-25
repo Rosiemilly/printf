46d62e98e15e084a7a9b62e4c566410848a4199c
Keisha <keishamtawodzera@gmail.com>
2023-07-25 03:45:49 -0700

    new update

--- functions1.c
+++ functions1.c
@@ -107,7 +107,7 @@ flags, 'x', width, precision, size));
 int print_hexa_upper(va_list types, char buffer[],
 int flags, int width, int precision, int size)
 {
-return (print_hexa(types, "0123456789ABCDEF", buffer,
+return (print_hexa(types, "0123456789ABCDEF", buffer, 
 flags, 'X', width, precision, size));
 }
 
